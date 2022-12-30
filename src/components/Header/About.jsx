16021e8b262683a41c955eed2785b651178b78e9
Birappa88 <birappa.mangod@gmail.com>
2022-12-30 12:01:47 +0530

    done

--- src/components/Header/About.jsx
+++ src/components/Header/About.jsx
@@ -4,7 +4,7 @@ import '../../css/aboutcss.css'
 const About = () => {
     return (
         <>
-            <article>
+            <article >
                 <h1 >WE ARE UFC GYM</h1>
                 <p>We are the original. The world leader. The trail-blazer of MMA-inspired fitness. Rooted in authenticity,
                     we are an extension of the Ultimate Fighting Championship® and proud to make the training programs and benefits of elite MMA athletes accessible to all,
